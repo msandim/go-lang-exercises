65b1bfccdc36ff1cba9746b2a14cb3885120e528
Miguel Sandim <migsandim@hotmail.com>
2018-05-21 01:28:30 +0100

    Create image.go

---
+++ image.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+	
+)
+
+type MyImage struct{
+	width, height int
+}
+
+func (i MyImage) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i MyImage) Bounds() image.Rectangle {
+	return image.Rect(0, 0, i.width - 1, i.height - 1)
+}
+
+func (i MyImage) At(x, y int) color.Color {
+	value := uint8(x^y)
+	return color.RGBA{value, value, 255, 255}
+}
+
+func main() {
+	m := MyImage{width: 256, height: 256}
+	pic.ShowImage(m)
+}
