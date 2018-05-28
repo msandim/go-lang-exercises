51a4670846a5332debc0e5d45dc3c340918c3b2b
msandim <migsandim@hotmail.com>
2018-05-29 00:47:55 +0100

    moved everything to directories

--- go-tour/binarytrees/binarytrees.go
+++ go-tour/binarytrees/binarytrees.go
@@ -1,8 +1,9 @@
 package main
 
-import(
-	"golang.org/x/tour/tree"
+import (
 	"fmt"
+
+	"golang.org/x/tour/tree"
 )
 
 // Walk walks the tree t sending all values
@@ -12,14 +13,15 @@ func Walk(t *tree.Tree, ch chan int) {
 	close(ch)
 }
 
+// WalkAux is an auxiliary function for Walk
 func WalkAux(t *tree.Tree, ch chan int) {
 	if t.Left != nil {
 		WalkAux(t.Left, ch)
 	}
-	
+
 	ch <- t.Value
-	
-	if t.Right != nil {	
+
+	if t.Right != nil {
 		WalkAux(t.Right, ch)
 	}
 }
@@ -29,22 +31,22 @@ func WalkAux(t *tree.Tree, ch chan int) {
 func Same(t1, t2 *tree.Tree) bool {
 	// Create channels to use the Walk function:
 	ch1, ch2 := make(chan int), make(chan int)
-	
+
 	// Walk the trees:
 	go Walk(t1, ch1)
 	go Walk(t2, ch2)
-	
+
 	for {
-		t1Value, t1Closed := <- ch1
-		t2Value, t2Closed := <- ch2
-		
+		t1Value, t1Closed := <-ch1
+		t2Value, t2Closed := <-ch2
+
 		// If both channels got terminated at the same time, then the sequence has the same size:
-		if (t1Closed && t2Closed) {
+		if t1Closed && t2Closed {
 			return t1Value == t2Value
 		}
-		
+
 		onlyOneChannelClosed := t1Closed != t2Closed
-		
+
 		// If one of the channels got closed, or the values are different:
 		if onlyOneChannelClosed || (t1Value != t2Value) {
 			return false
@@ -55,20 +57,20 @@ func Same(t1, t2 *tree.Tree) bool {
 func main() {
 	// Create channel to test Walk:
 	ch := make(chan int)
-	
+
 	// Walk the tree:
 	go Walk(tree.New(1), ch)
-	
+
 	for value := range ch {
 		fmt.Println(value)
 	}
-	
+
 	if Same(tree.New(1), tree.New(1)) {
 		fmt.Println("TEST 1 PASSED")
 	} else {
 		fmt.Println("TEST 1 FAILED")
 	}
-	
+
 	if Same(tree.New(1), tree.New(2)) {
 		fmt.Println("TEST 2 FAILED")
 	} else {
